12ccf614d265898b57229d549ab33fbf3f672d11
Khan, Adnan Ahmad <Adnan.Ahmad@afiniti.com>
2021-05-10 19:20:59 +0500

    chabges

--- load.py
+++ load.py
@@ -5,20 +5,26 @@ def load_into_table(data, table_name):
     """
     Load data into table of desired name
     """
-    con = util.get_connection("datawarehouse.db")
-    con.execute("create table features (source_id, name, category, length, scaled_length, created_at)")
 
-    export = []
+    try:
+        con = util.get_connection("datawarehouse.db")
+        con.execute("create table features (source_id, name, category, length, scaled_length, created_at)")
 
-    cnt = 0
-    for i in data:
-        cnt+=1
+        export = []
 
-        export.append((i["source_id"], i["data"]["name"], i["data"]["category"], i["data"]["length"], i["data"]["created_at"], i["scaled_legnth"],))
+        cnt = 0
+        for i in data:
+            cnt+=1
 
-        # adding data in bulks
-        if(cnt%100==0): 
-            con.executemany("insert into features(source_id, name, category, length, scaled_length, created_at) values (?,?,?,?,?,?)", export)
-            export = []
-        
+            export.append((i["source_id"], i["data"]["name"], i["data"]["category"], i["data"]["length"], i["data"]["created_at"], i["scaled_legnth"],))
+
+            # adding data in bulks
+            if(cnt%100==0): 
+                print(export)
+                con.executemany("insert into features(source_id, name, category, length, scaled_length, created_at) values (?,?,?,?,?,?)", export)
+                export = []
             
+    except:
+        print("Exception occured")
+    finally:
+        con.close()
