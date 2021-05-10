f50cc370fd4c42e9dcac0e1487fd8c0f478dd0e7
Khan, Adnan Ahmad <Adnan.Ahmad@afiniti.com>
2021-05-10 18:50:14 +0500

    chabges

--- transform.py
+++ transform.py
@@ -96,7 +96,7 @@ def apply_scale_by_category(data):
     for i in data:
         cat = i["data"]["category"]
         scale = float(util.get_scale_by_category(con, cat)[0])
-        i["scaled_legnth"] = scale * i["data"]["value"]
+        i["scaled_legnth"] = scale * i["data"]["length"]
     return row
 
 
