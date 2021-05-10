f91d2540125b5a31aaf3165f8530fcbf9b770c59
Khan, Adnan Ahmad <Adnan.Ahmad@afiniti.com>
2021-05-10 17:05:40 +0500

    changes

--- ETL.py
+++ ETL.py
@@ -2,6 +2,7 @@ from utils.logger import LOG
 import extract
 import json
 from os.path import join, dirname
+from utils import util
 
 # Global variables for interacting with filesystems
 dirname = dirname(__file__)
@@ -17,11 +18,14 @@ if __name__ == "__main__":
     Main function to be run
     """
 
+    # extract file from remote source
     filename = extract.extract_html(env["file_uri"])
+    
+    # convert to dictionary
     with open(join(downloads_folder, filename)) as json_file:
         data = json.load(json_file)
 
-    print(data[:3])
+    util.pretty(data)    
 
     
     
--- utils/util.py
+++ utils/util.py
@@ -47,3 +47,13 @@ def get_scale_by_category(conn, category):
 
     row = cur.fetchone()
     return row
+
+
+
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
