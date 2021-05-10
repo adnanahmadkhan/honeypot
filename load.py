2e205b4972e714f13b44119f210dd85471712f51
Khan, Adnan Ahmad <Adnan.Ahmad@afiniti.com>
2021-05-10 19:35:38 +0500

    chabges

--- load.py
+++ load.py
@@ -8,7 +8,7 @@ def load_into_table(data):
 
     try:
         con = util.get_connection("datawarehouse.db")
-        con.execute("create table features (source_id, name, category, length, scaled_length, created_at)")
+        con.execute("create table if not exists features (source_id, name, category, length, scaled_length, created_at)")
 
         export = []
 
